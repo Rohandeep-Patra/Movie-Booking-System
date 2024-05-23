d7ebf43af83743875ba43499d4ba2da4a8b61ef0
Rohandeep Patra <rohandp100@gmail.com>
2024-05-23 10:59:17 +0530

    frontend setup

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-poppins underline">Hello world!</h1>
+      <p className=" font-poppins">Rohandeep Patra</p>
+    </>
+  );
+}
+
+export default App;
